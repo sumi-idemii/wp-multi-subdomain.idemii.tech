f201804239c9750f5c92ba9a6feb303ff52c418c
sumi-idemii <sumi@idemii.jp>
2025-11-26 13:41:37 +0900

    Update archive.php template

---
+++ public/wp-content/themes/MainSite/archive.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package nagoya-u-en
+ */
+
+
+get_header();
+?>
+<p>テンプレート：/archive.php</p>
+
+<?php
+get_footer();
---
+++ public/wp-content/themes/MainSite/single.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Articles(News)詳細ページ
+ */
+
+get_header();
+?>
+
+<p>テンプレート：/single.php</p>
+
+<?php
+get_footer(); 
