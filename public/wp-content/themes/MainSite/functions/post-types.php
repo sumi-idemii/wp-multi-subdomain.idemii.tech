f33e7737ef00e9d19a4f2e59476bf4ca25e048a3
sumi-idemii <sumi@idemii.jp>
2025-12-26 02:12:35 +0900

    投稿タイプキーに「_setting」を含む場合、投稿一覧と新規投稿のメニューを非表示にする処理を追加

--- public/wp-content/themes/MainSite/functions/post-types.php
+++ public/wp-content/themes/MainSite/functions/post-types.php
@@ -8,3 +8,35 @@ function disable_default_post_type() {
 }
 add_action('admin_menu', 'disable_default_post_type');
 
+/**
+ * 投稿タイプキーに「_setting」を含む場合、投稿一覧と新規投稿のメニューを非表示にする
+ * タクソノミーのメニューのみ表示する
+ */
+function hide_setting_post_type_menus() {
+    global $submenu;
+    
+    // 登録されているすべての投稿タイプを取得
+    $post_types = get_post_types(array('public' => true, '_builtin' => false), 'objects');
+    
+    foreach ($post_types as $post_type) {
+        // 投稿タイプキーに「_setting」が含まれている場合
+        if (strpos($post_type->name, '_setting') !== false) {
+            $menu_slug = 'edit.php?post_type=' . $post_type->name;
+            
+            // サブメニューが存在する場合
+            if (isset($submenu[$menu_slug])) {
+                foreach ($submenu[$menu_slug] as $key => $menu_item) {
+                    // 投稿一覧（親メニューと同じスラッグ）または新規投稿のサブメニューを非表示
+                    if (isset($menu_item[2])) {
+                        $submenu_slug = $menu_item[2];
+                        if ($submenu_slug === $menu_slug || $submenu_slug === 'post-new.php?post_type=' . $post_type->name) {
+                            unset($submenu[$menu_slug][$key]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+add_action('admin_menu', 'hide_setting_post_type_menus', 999);
+
